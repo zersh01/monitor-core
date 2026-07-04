87e7891505f9d9aad7ea5dfdf60c53bccdad2ac6
zersh <zersh@mail.ru>
2026-07-05 01:23:32 +0300

    Add dynamic per-NUMA node SUnreclaim memory metrics for Linux

--- gmond/modules/memory/mod_mem.c
+++ gmond/modules/memory/mod_mem.c
@@ -1,22 +1,66 @@
 #include <gm_metric.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <apr_strings.h>
 #include <libmetrics.h>
 
 mmodule mem_module;
-
+static int num_numa_nodes = 0;
+static Ganglia_25metric *dynamic_metric_info = NULL;
 
 static int mem_metric_init ( apr_pool_t *p )
 {
-    int i;
+    int i, j;
+    struct dirent *entry;
+    DIR *dir;
 
     libmetrics_init();
 
     for (i = 0; mem_module.metrics_info[i].name != NULL; i++) {
-        /* Initialize the metadata storage for each of the metrics and then
-         *  store one or more key/value pairs.  The define MGROUPS defines
-         *  the key for the grouping attribute. */
         MMETRIC_INIT_METADATA(&(mem_module.metrics_info[i]),p);
         MMETRIC_ADD_METADATA(&(mem_module.metrics_info[i]),MGROUP,"memory");
     }
+
+    #ifdef LINUX
+    /* Count NUMA nodes dynamically */
+    dir = opendir("/sys/devices/system/node");
+    if (dir) {
+        while ((entry = readdir(dir)) != NULL) {
+            if (strncmp(entry->d_name, "node", 4) == 0 && entry->d_name[4] >= '0' && entry->d_name[4] <= '9') {
+                num_numa_nodes++;
+            }
+        }
+        closedir(dir);
+    }
+
+    if (num_numa_nodes > 0) {
+        int base_count = 0;
+        while (mem_module.metrics_info[base_count].name != NULL) base_count++;
+
+        dynamic_metric_info = apr_pcalloc(p, (base_count + num_numa_nodes + 1) * sizeof(Ganglia_25metric));
+        memcpy(dynamic_metric_info, mem_module.metrics_info, base_count * sizeof(Ganglia_25metric));
+
+        for (j = 0; j < num_numa_nodes; j++) {
+            char *name = apr_pstrcat(p, "mem_SUnreclaim_node", apr_itoa(p, j), NULL);
+            char *desc = apr_pstrcat(p, "Amount of un-reclaimable slab memory on NUMA node ", apr_itoa(p, j), NULL);
+            
+            dynamic_metric_info[base_count + j].key = base_count + j;
+            dynamic_metric_info[base_count + j].name = name;
+            dynamic_metric_info[base_count + j].msg_size = UDP_HEADER_SIZE+8;
+            dynamic_metric_info[base_count + j].type = GANGLIA_VALUE_FLOAT;
+            dynamic_metric_info[base_count + j].units = "KB";
+            dynamic_metric_info[base_count + j].slope = "both";
+            dynamic_metric_info[base_count + j].fmt = "%.0f";
+            dynamic_metric_info[base_count + j].desc = desc;
+            
+            MMETRIC_INIT_METADATA(&(dynamic_metric_info[base_count + j]), p);
+            MMETRIC_ADD_METADATA(&(dynamic_metric_info[base_count + j]), MGROUP, "memory");
+        }
+        mem_module.metrics_info = dynamic_metric_info;
+    }
+    #endif
 
     return 0;
 }
@@ -28,10 +72,8 @@ static void mem_metric_cleanup ( void )
 static g_val_t mem_metric_handler ( int metric_index )
 {
     g_val_t val;
+    int base_count = 10; 
 
-    /* The metric_index corresponds to the order in which
-       the metrics appear in the metric_info array
-    */
     switch (metric_index) {
     case 0:
         return mem_total_func();
@@ -55,19 +97,33 @@ static g_val_t mem_metric_handler ( int metric_index )
     case 9:
         return mem_available_func();
 #endif
-#if HPUX
-    case 7:
-        return mem_arm_func();
-    case 8:
-        return mem_rm_func();
-    case 9:
-        return mem_avm_func();
-    case 10:
-        return mem_vm_func();
-#endif
+    default:
+        #ifdef LINUX
+        if (metric_index >= base_count && metric_index < (base_count + num_numa_nodes)) {
+            int node_id = metric_index - base_count;
+            char path[128];
+            FILE *fp;
+            
+            val.f = 0.0;
+            snprintf(path, sizeof(path), "/sys/devices/system/node/node%d/meminfo", node_id);
+            fp = fopen(path, "r");
+            if (fp) {
+                char line[256];
+                while (fgets(line, sizeof(line), fp)) {
+                    if (strstr(line, "SUnreclaim:")) {
+                        char *p = strchr(line, ':');
+                        if (p) val.f = atof(p + 1);
+                        break;
+                    }
+                }
+                fclose(fp);
+            }
+            return val;
+        }
+        #endif
+        break;
     }
 
-    /* default case */
     val.f = 0;
     return val;
 }
@@ -86,14 +142,7 @@ static Ganglia_25metric mem_metric_info[] =
     {0, "mem_slab",    180, GANGLIA_VALUE_FLOAT, "KB", "both", "%.0f", UDP_HEADER_SIZE+8, "Amount of in-kernel data structures cache"},
     {0, "mem_available",    180, GANGLIA_VALUE_FLOAT, "KB", "both", "%.0f", UDP_HEADER_SIZE+8, "Estimate of how much memory is available"},
 #endif
-#if HPUX
-    {0, "mem_arm",     180, GANGLIA_VALUE_FLOAT, "KB", "both", "%.0f", UDP_HEADER_SIZE+8, "mem_arm"},
-    {0, "mem_rm",      180, GANGLIA_VALUE_FLOAT, "KB", "both", "%.0f", UDP_HEADER_SIZE+8, "mem_rm"},
-    {0, "mem_avm",     180, GANGLIA_VALUE_FLOAT, "KB", "both", "%.0f", UDP_HEADER_SIZE+8, "mem_avm"},
-    {0, "mem_vm",      180, GANGLIA_VALUE_FLOAT, "KB", "both", "%.0f", UDP_HEADER_SIZE+8, "mem_vm"},
-#endif
     {0, NULL}
-
 };
 
 mmodule mem_module =
@@ -104,4 +153,3 @@ mmodule mem_module =
     mem_metric_info,
     mem_metric_handler,
 };
-
